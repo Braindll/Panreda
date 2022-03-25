405b791107ec0dd50629f5daf0a2c73a3990d404
brainDll <alpertektas@gmail.com>
2022-03-25 09:12:35 +0300

    1

---
+++ prom.py
@@ -0,0 +1,11 @@
+import manage
+
+def arti(self):
+    global arttir
+    arttir+=1
+    buton.config(text=arttir)
+    if alper==20:
+        window.destroy()
+
+def closeWindow(self):
+    window.destroy()
