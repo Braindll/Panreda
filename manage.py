ec2cd30f5d3c92270591d5bfd5e350a7eb7c2931
brainDll <alpertektas@gmail.com>
2022-03-25 09:12:47 +0300

    1

--- manage.py
+++ manage.py
@@ -1,22 +1,31 @@
 from tkinter import *
+from os import system
 
+arttir=0
+
+def arti():
+    global arttir
+    arttir+=1
+    buton.config(text=arttir)
+    if arttir==20:
+        window.destroy()
+
+def closeWindow():
+    window.destroy()
+
+system("clear")
 window=Tk()
 window.geometry("800x600")
 window.title("Helllooo Beauty")
 window.config(background="white")
-alper=1
 
-def arti():
-    global alper
-    alper+=1
 
-buton=Button(window,command=arti,text=alper,width=10,padx=15,pady=15)
+buton=Button(window,command=arti,text=arttir,width=10,padx=15,pady=15)
 buton.pack(padx=15,pady=15)
 
-buton1=Button(window,command=window.destroy,text="LaLa Paşa",width=10,padx=15,pady=15)
+buton1=Button(window,command=closeWindow,text="LaLa Paşa",width=10,padx=15,pady=15)
 buton1.pack(padx=15,pady=15)
 
-while True:
-    window.update()
-    buton.config(text=alper)
+window.mainloop()
+    
     
